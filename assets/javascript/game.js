715de87b18089b4497d0d9f9c9c4bacfbc8b9275
Huan Nguyen <huancal1984@gmail.com>
2019-10-03 17:49:38 -0700

    pseudo codes added

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+// need on key event that records any letter user inputs
+// need response from computer than randomly selects a key
+// Condition where if letter selected by user is same as user its a win 
+// condition where if letter selected by user is different than computer its a loss
+// display the wins and losses to html
+// display how many guesses left
+// display how many guesses so far
