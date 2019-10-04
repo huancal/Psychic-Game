b1bb6b5269b4ead3506b7afcc6611b9687a819d7
Huan Nguyen <huancal1984@gmail.com>
2019-10-03 19:04:30 -0700

    javascript added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,3 +5,49 @@
 // display the wins and losses to html
 // display how many guesses left
 // display how many guesses so far
+
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ]
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+var wins = 0
+var losses = 0
+var guesses_left = 9
+var Guesses_so_far = []
+
+var playerGuess
+
+// this function is when a user inputs a key
+document.onkeyup = function (event) {
+    // convert to lower in case user inputs upper case
+    playerGuess = event.key.toLowerCase();
+    guesses_left--;
+    Guesses_so_far.push(playerGuess);
+    // re-generating computer tries 
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    document.querySelector('#your_guesses_so_far').innerHTML = playerGuess.join(' , ');
+
+    // in case a user wins the game
+    if (playerGuess === computerGuess) {
+        wins++
+        document.querySelector('#wins').innerHTML = wins
+        // resets all guesses and guesses so far variables
+        reset_game()
+    }
+    if (guesses_left === 0) {
+        losses++
+        document.querySelector('#losses').innerHTML = losses
+        reset_game()
+    }
+
+
+
+}
+
+var reset_game = function () {
+    guesses_left = 9
+    Guesses_so_far = []
+    document.querySelector('#guesses_left').innerHTML = guesses_left
+    document.querySelector('#your_guesses_so_far').innerHTML = Guesses_so_far
+
+}
